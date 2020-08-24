736b7c05cc36a4e05de07258ab43b61b942ddecf
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-24 14:44:06 -0700

    Add project files.

---
+++ Online Assessment/AmazonFreshPromotion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Assessment
+{
+    public class AmazonFreshPromotion
+    {
+        //Main
+        /*
+        List<List<string>> codeList = new List<List<string>>
+            {
+                new List<string> { "apple", "apple"},
+                new List<string> { "banana", "anything", "banana"}
+            };
+
+        List<string> shoppingCart = new List<string>
+            {
+                "orange", "apple", "apple", "banana", "orange", "banana"
+                //"banana", "orange", "banana", "apple", "apple"
+            };
+
+        int isWinner = IsWinner(codeList, shoppingCart);
+        */
+
+        public int IsWinner(List<List<string>> codeList, List<string> shoppingCart)
+        {
+            if (codeList.Count == 0) return 1;
+            if (shoppingCart.Count == 0) return 0;
+
+            int i = 0, j = 0;
+            for (int a = 0; a < shoppingCart.Count; a++)
+            {
+                if (codeList[i][j] == shoppingCart[a] || codeList[i][j] == "anything")
+                {
+                    j++;
+                    if (j == codeList[i].Count)
+                    {
+                        i++;
+                        j = 0;
+                    }
+
+                    if (i == codeList.Count) return 1;
+                }
+                else
+                {
+                    j = codeList[i][0] == "anything" ? 1 : 0;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Online Assessment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Online_Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PairString> input = new List<PairString> {
+                new PairString("item1", "item2"),
+                new PairString("item3", "item4"),
+                new PairString("item4", "item5")
+            };
+
+            List<string> result = LargestItemAssociation(input);
+        }
+
+        public static List<string> LargestItemAssociation(List<PairString> itemAssociation)
+        {
+
+        }
+    }
+
+    public class PairString
+    {
+        public string First;
+        public string Second;
+
+        public PairString(string first, string second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+}
